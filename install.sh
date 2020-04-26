90e63d45dfad72674c75c57477ad2e30d0d0c626
Özkan ŞEN <ozkansen@gmail.com>
2020-04-26 12:47:03 +0300

    fs inotify

--- install.sh
+++ install.sh
@@ -141,3 +141,10 @@ install_vscode () {
     sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
     sudo apt-get install code
 }
+
+
+
+
+# System Settings
+# Update system watch file limits
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
