4e54ecaf0571fecbf96d947581dd34d131ef18e9
Özkan ŞEN <ozkansen@gmail.com>
2020-05-10 19:39:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -94,7 +94,8 @@ install_python_packages() {
     jupyter \
     jupyter_contrib_nbextensions \
     jupyter_nbextensions_configurator
-
+    
+    source ~/.profile
     jupyter contrib nbextension install --user
     jupyter nbextensions_configurator enable --user
 }
@@ -107,7 +108,7 @@ install_sublime() {
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
     sudo apt update
-    sudo install sublime-merge sublime-text -y
+    sudo apt install sublime-merge sublime-text -y
 }
 install_sublime;
 
@@ -169,6 +170,7 @@ install_vscode () {
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
     sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt update
     sudo apt-get install code
     rm -f packages.microsoft.gpg
 }
